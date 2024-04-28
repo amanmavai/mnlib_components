d0e01e27c4d9e35ad31b64a5e7c46d699a9458fd
Aman Mavai <amanmavai@gmail.com>
2024-04-28 08:03:40 +0530

    removed css injection

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,10 @@ import {glob} from "glob";
 import {peerDependencies} from "./package.json";
 
 import dts from "vite-plugin-dts";
-import {libInjectCss} from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({include: ["lib"]})],
+  plugins: [react(), dts({include: ["lib"]})],
   build: {
     minify: false,
     lib: {
