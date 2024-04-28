9ace79cf3bf864549ddf600cca775f4bebdfaa11
Aman Mavai <amanmavai@gmail.com>
2024-04-28 21:52:30 +0530

    export combobox component

--- lib/main.ts
+++ lib/main.ts
@@ -4,6 +4,7 @@ import "./styles/tailwind.css";
 export {HelloAtom} from "./components/custom/HelloAtom";
 export {Debug} from "./components/custom/Debug";
 export {Header, Footer} from "./components/custom/Layout";
+export {Combobox} from "./components/custom/Combobox";
 
 // shadcn components
 export {Button} from "./components/ui/button";
