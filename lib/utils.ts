1ee87ee1b552357b9904bbbea965d0d3655a2f6b
Aman Mavai <amanmavai@gmail.com>
2024-05-03 13:05:05 +0530

    extend twMerge

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {type ClassValue, clsx} from "clsx";
+import {extendTailwindMerge} from "tailwind-merge";
+
+export const twMerge = extendTailwindMerge({
+  prefix: "tw-",
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
